637c61524aa161f724f2f6598eb4abae84ab0789
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-14 17:22:04 +0530

    Invoke book_ticket and cancel_ticket from movie_booking_app class

--- lib/movie_booking_app.rb
+++ lib/movie_booking_app.rb
@@ -5,6 +5,9 @@ require 'tty-prompt'
 
 require_relative 'parsers/file_parser'
 require_relative 'services/movies_importer_service'
+require_relative 'services/book_ticket_service'
+require_relative 'helpers/user_interface'
+require_relative 'services/cancel_ticket_service'
 
 # The class is the main entry point of the application.
 class MovieBookingApp
@@ -16,12 +19,27 @@ class MovieBookingApp
     setup
   end
 
+  def book_ticket(show)
+    result = BookTicketService.call(show: show)
+    tickets << result[:ticket] if result[:success]
+
+    result
+  end
+
+  def cancel_ticket(ticket)
+    CancelTicketService.call(ticket: ticket)
+  end
+
   def prompt
     @prompt ||= TTY::Prompt.new(interrupt: :exit, active_color: :cyan)
   end
 
   def movies
     @movies ||= []
+  end
+
+  def tickets
+    @tickets ||= []
   end
 
   private
--- spec/movie_booking_app_spec.rb
+++ spec/movie_booking_app_spec.rb
@@ -34,4 +34,48 @@ RSpec.describe MovieBookingApp do
       expect(subject.movies).to be_empty
     end
   end
+
+  describe '#tickets' do
+    it 'returns an array' do
+      expect(subject.tickets).to be_an(Array)
+    end
+
+    it 'returns an empty array by default' do
+      expect(subject.tickets).to be_empty
+    end
+  end
+
+  describe '#book_ticket' do
+    let(:show) { instance_double('Show') }
+    let(:ticket) { instance_double('Ticket') }
+    let(:success_result) { { success: true, ticket: ticket, message: '🎉 Ticket booked successfully! 🎉' } }
+
+    before do
+      allow(BookTicketService).to receive(:call).with(show: show).and_return(success_result)
+    end
+
+    it 'calls BookTicketService and adds the ticket to the list of tickets if booking is successful' do
+      subject.book_ticket(show)
+      expect(BookTicketService).to have_received(:call).with(show: show)
+      expect(subject.tickets).to contain_exactly(ticket)
+    end
+  end
+
+  describe '#cancel_ticket' do
+    let(:ticket) { instance_double('Ticket') }
+    let(:success_result) { { success: true, message: '🎉 Ticket canceled successfully 🎉' } }
+
+    before do
+      allow(subject).to receive(:tickets).and_return([ticket])
+      allow(CancelTicketService).to receive(:call).with(ticket: ticket).and_return(success_result)
+    end
+
+    it 'calls CancelTicketService' do
+      result = subject.cancel_ticket(ticket)
+
+      expect(CancelTicketService).to have_received(:call).with(ticket: ticket)
+      expect(result[:success]).to be_truthy
+      expect(result[:message]).to eq('🎉 Ticket canceled successfully 🎉')
+    end
+  end
 end
